d4392551c6f1fa78f45abab5832d8798ba02da20
pranav-gupta12 <136360780+pranav-gupta12@users.noreply.github.com>
2024-12-10 13:01:52 +0530

    Delete src directory

--- src/main/java/com/connectly/config/JwtConstant.java
+++
@@ -1,9 +0,0 @@
-package com.connectly.config;
-
-public class JwtConstant {
-	
-	public static String JWT_HEADER = "Authorization";
-	
-	public static String SECRET_KEY = "fjirengnguithbitrgregvafvrgvlkfeagtrheyjt";
-
-}
--- src/main/java/com/connectly/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.connectly.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.connectly.models.Comment;
-
-public interface CommentRepository  extends JpaRepository<Comment, Integer>{
-
-}
